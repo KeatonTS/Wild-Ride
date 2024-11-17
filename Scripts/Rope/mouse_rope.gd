f4163430022fa990ca821cb731a5dbe106f4055a
tap799 <148083237+tap799@users.noreply.github.com>
2024-11-16 20:03:19 -0500

    Created rope and added graphics for road

--- Scripts/Rope/mouse_rope.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@export var rope : Rope2D
-
-func _ready():
-	rope.bind_to_node(self, 0)
-
-func _process(delta):
-	global_position = get_global_mouse_position()
---
+++ Scripts/rope.gd
@@ -0,0 +1,18 @@
+extends Node2D
+# Create a rope with 10 segments
+#var rope_length = 3
+#var segment_distance = 2  # Distance between each rope segment
+#
+#func create_rope(start_position):
+	#var prev_body = null
+	#for i in range(rope_length):
+		#var body = RigidBody2D.new()
+		#body.position = start_position + Vector2(i * segment_distance, 0)
+		#add_child(body)
+#
+	#if prev_body:
+		#var joint = PinJoint2D.new()
+		#joint.body_a = prev_body
+		#joint.body_b = body
+		#add_child(joint)
+		#prev_body = body
